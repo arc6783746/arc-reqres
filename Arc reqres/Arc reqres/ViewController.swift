fa0351fd92105075188b943a8f520fe0f248f288
Arcael <arcael@Arcaels-MacBook-Pro.local>
2020-09-09 16:07:49 +0300

    initial commit

---
+++ Arc reqres/Arc reqres/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Arc reqres
+//
+//  Created by Arcael on 9/9/20.
+//  Copyright © 2020 Arcael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
